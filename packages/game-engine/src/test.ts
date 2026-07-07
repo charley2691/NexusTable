cf9fdc8a5e4b0b3786a6725ef8fb35178e146546
charley <criches2691@gmail.com>
2026-07-07 13:12:48 +0100

    Remove temporary engine test files

--- packages/game-engine/src/test.ts
+++
@@ -1,9 +0,0 @@
-import { EventBus } from "./EventBus";
-
-const bus = new EventBus();
-
-bus.on("hello", (message) => {
-  console.log(message);
-});
-
-bus.emit("hello", "NexusTable Engine Online");
