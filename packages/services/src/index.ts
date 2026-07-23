98ab61a2c15980892f6c4d705dadf833fe893f50
charley <criches2691@gmail.com>
2026-07-23 04:53:24 +0100

    refactor: add services application layer

---
+++ packages/services/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./assets/index.js";
+export * from "./campaigns/index.js";
+export * from "./networking/index.js";
+export * from "./permissions/index.js";
+export * from "./search/index.js";
+export * from "./users/index.js";
